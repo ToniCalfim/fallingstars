4c055e16fab8afef776724089f0e1aaf12c3d149
Raven Code <soulidbrasil@gmail.com>
2020-01-26 22:05:59 -0300

    Refactoring has been done, it works well

--- index.js
+++ index.js
@@ -1,137 +1,162 @@
-﻿"use strict"
-
-/**
-	Creation Date: 2020 Jan, 25
-	Last Modified: 2020 Jan, 26
-
-	Programers: @RavenCodde, @ToniCalfim
-
-	Separation of Concerns:
-		- [ ] Presentation
-		- [ ] Logic & Data
-		- [X] Input
-		- [X] Network
-
-	Os links http://bit.ly/37mKmGC e http://bit.ly/2TX2R07 são referências de commandos canvas.
-
-	Our Proposal
-	We've started this code to...
-									...have fun
-									...apply basic uses of JS Language on Canvas.
-									...consolidate JS coding paradigmas
-									...code remotely
-									...test developing tools like console browser, VSCode, etc...
+﻿/**
+ * 
+ * Creation Date: 2020 Jan, 25
+ * Last Modified: 2020 Jan, 26
+ * 
+ *  Overview on Separation of Concerns:
+ * 		- [    implemented  ] Presentation layer
+ * 		- [    implemented  ] Logic & Data  layer
+ * 		- [No implementation] Input layer
+ * 		- [No implementation] Network layer
+ * 
+ * 	Our Proposal
+ * 	We've started this code to...
+ * 									...have fun
+ * 									...apply basic uses of JS Language on Canvas.
+ * 									...consolidate JS coding paradigmas
+ * 									...code remotely
+ * 									...test developing tools like console browser, VSCode, etc...
+ * 									...practice JSDoc documentation - see link reference below
+ * 									...practice English on a real envitonment
  */
 
+ /** *************************************************************************************************************** */
+ /** 																												 */
+ /** *************************************************************************************************************** */
+ 
+/**
+ * @file OMG, it's full of stars...
+ * @fileoverview Check "Our Proposal" aforementioned
+ * @supported Tested on Chrome so far
+ * @version 1.0.0.0
+ * 
+ * @author Erivan "Raven Codde" Cerqueira <...@gmail.com>
+ * @author Toni Calfim <751127@gmail.com>
+ * 
+ * @see {@link JSDoc} for further information...
+ * @see {@link http://bit.ly/38fOjwE|JSDoc}
+ *
+ * @see {@link ConstLetVar} for further information...
+ * @see {@link http://bit.ly/38j2UaO|ConstLetVar}
+ *
+ * @see {@link Canvas Commands} for further information...
+ * @see {@link http://bit.ly/37mKmGC|Canvas Commands}
+ *
+ * @see {@link More Canvas Commands} for further information...
+ * @see {@link http://bit.ly/2TX2R07|More Canvas Commands}
+ *
+ * @todo Ellaborate on it
+ * @license Pode usar à vontade!
+ */
+		
+const STARS_DIAMETER = 2
+		
+const COLOR_PALLETE = [
+	'white', 'yellow', 'lightyellow', 'blue', 'lightblue',      'darkblue', 'red', 'darkred', 'orange', 'darkorange'
+	//'turquoise', 'purple', 'green',  'darkturquoise', 'lightgreen', 'darkgreen' , ,
+]
+
 // Instancia canvas como referenciada no HTML que chama esse script
-const myCanvas = document.getElementById("myCanvas")
-const context = myCanvas.getContext("2d")
-
-// Randomiza um número inteiro dentro de um intervalo
-function randomInt(min, max) {
-	return min + Math.floor((max - min) * Math.random());
-}
-
-// Stars' Data Structure.
-// It's empty to be filled by starsData.stars
+const canvas = document.getElementById("canvas")
+const context2D = canvas.getContext("2d")
+		
 const starsData = {
 	stars: {
-		// Object Stars Definitions goes here...
+	// Object Stars Definitions goes here...
 	}
 }
+		
+function getRandomNumber(min, max) {
+    return min + Math.floor((max - min) * Math.random());
+}
 
-const STARS_DIAMETER = 2
+const createStarObject =  () => {
+    
+    let positionX = getRandomNumber(0, 650);
+    let positionY = getRandomNumber(0,125);
+	let diameter  = STARS_DIAMETER;
+	
+	let pulsing  = 0;
+    let blinking = 0;
+	
+	let timeToFall      =   getRandomNumber(0,1000);  // See wait = http://bit.ly/2TXHvjy
+	let velocityToFall  =	getRandomNumber(1, 5); // we need to visually test smoothness - try no to use only integers
+	let directionToFall =	getRandomNumber(-1, 1);
+            
+    let color    = COLOR_PALLETE[
+        getRandomNumber(0, COLOR_PALLETE.length)
+    ];
+    
+    return {
 
-const COLOR_PALLETE = ['white', 'yellow', 'lightyellow', 'blue', 'lightblue', 'darkblue', 'red', 'darkred', 'orange', 'darkorange']
-						//'turquoise', 'purple', 'green',  'darkturquoise', 'lightgreen', 'darkgreen' , ,
+  	     	x: positionX,
+            y: positionY,
+			diameter,   
+			pulsing,
+			blinking,
+			timeToFall,
+			velocityToFall,
+			directionToFall,
+            color,
+    }
+}
 
-// Define numbers of stars
-for ( let i = 0; i < 150; i++ ) {
-
+for ( let i = 0; i < 25; i++ ) {
 	const newStar = 'star' + i;
-	
-	starsData.stars[newStar] = {
-		x:					randomInt(0, 650),
-		y:					randomInt(0, 125),
-		blinking:			0,	// boolean
-		pulsing:			0,	// boolean
-		timeToFall:			randomInt(0,1000),  //randomInt(0, 3) * 1000,
-		velocityToFall:		randomInt(1, 5), // we need to visually test smoothness - try no to use only integers
-		directionToFall:	randomInt(-1, 1),
-		diameter:			STARS_DIAMETER,
-		color:				COLOR_PALLETE[randomInt(0, COLOR_PALLETE.length)]
+	starsData.stars[newStar] = createStarObject();
+}	
+
+function update() {
+		
+	const stars = starsData.stars;
+
+	for ( const starID in stars ) {
+			
+		const currentStar = stars[starID];			
+		const currentTimeToFall = currentStar.timeToFall;
+
+		if ( currentTimeToFall != 0 ) {
+
+			currentStar.timeToFall = currentTimeToFall - 1;
+
+		} else {
+
+			const currentVelocityToFall = currentStar.velocityToFall
+			const currentAngleToFall = currentStar.directionToFall
+
+			const currentPositionX = currentStar.x;
+			const currentPositionY = currentStar.y;
+
+			currentStar.x = currentPositionX + 1 * currentAngleToFall;
+			currentStar.y = currentPositionY + currentVelocityToFall;				
+
+		}
 	}
 }
-
-// FactoryCreateS
-// const factoryCreateStar = ({x, y, blinking, pulsing, timeToFall, velocityToFall, diameter, color}) => ({
-	// 	x,
-	// 	y,
-	// 	blinking,
-	// 	pulsing,
-	// 	timeToFall,
-	// 	angleToFall,
-	// 	velocityToFall,
-	// 	diameter,
-	// 	color
-	//   });
 	
-	//   const ravenStar = factoryCreateStar( {x:100, y:60, blinking:0, pulsing:0, timeToFall:0, angleToFall:0, velocityToFall:0, diameter: 20, color:'green' } )
-	//createUser(  { userName: 'echo', avatar: 'echo.png' }   ) ;
-	
-	
-	
-	
-	function update() {
-		let x = randomInt(0, 650)
-		let y = randomInt(0, 125)	
+function drawStars() {
 
-		//var input = {'x': 0, 'y': 0};
-        //input['x'] += 1;
-		//rectanglePositionX = ameiva.input['x'];
+	context2D.clearRect(0, 0, context2D.canvas.width, context2D.canvas.height) // Clear canvas
 
-		// x = 10, y = 10
-		// 
+	const stars = starsData.stars;
+		
+	for ( const starID in stars ) {
+		
+		context2D.beginPath()
+		context2D.fillStyle =  stars[starID].color
+		context2D.arc(stars[starID].x, stars[starID].y, STARS_DIAMETER, 0, 2 * Math.PI)
+		context2D.fill()
+		context2D.closePath()
 
-		for ( const data in starsData.stars ) {
-			const timeFall = starsData.stars[data].timeToFall;
-
-			if(timeFall == 0){
-				const velocityFall = starsData.stars[data].velocityToFall
-				const angleFall = starsData.stars[data].directionToFall
-
-				const currentPositionX = starsData.stars[data].x;
-				const currentPositionY = starsData.stars[data].y;
-
-				starsData.stars[data].x = currentPositionX + 1 * angleFall;
-				starsData.stars[data].y = currentPositionY + velocityFall;
-			}else {
-				starsData.stars[data].timeToFall = timeFall -1;
-			}
-		}
-
-		// starsData.stars['star6'].y = starsData.stars['star6'].y +1;
-		// starsData.stars['star6'].x = starsData.stars['star6'].x -1;
 	}
-	
-	function drawStars() {
-		update();
-		// Laço para percorrer a estrutura de dados starsData
 
-		context.clearRect(0, 0, context.canvas.width, context.canvas.height) // Dá um clear no canvas	
-
-		for ( const eachStar in starsData.stars ) {
-			console.log( starsData.stars[eachStar].color )
-			 context.beginPath()
-			 context.fillStyle =  starsData.stars[eachStar].color  ///COLOR_PALLETE[randomInt(0, COLOR_PALLETE.length)]
-			 context.arc(starsData.stars[eachStar].x, starsData.stars[eachStar].y, STARS_DIAMETER, 0, 2 * Math.PI)
-			 context.fill()
-			 context.closePath()
-		}
-
-
-	// See wait = http://bit.ly/2TXHvjy
-	requestAnimationFrame(drawStars)	
 }
 
-drawStars()
+function animateLoop(){
+	
+	update();
+	drawStars();
+	requestAnimationFrame(animateLoop);	
+}
+
+animateLoop();
